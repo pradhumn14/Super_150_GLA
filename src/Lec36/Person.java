de910c22905583c265efd8b0e5395125ab78a0ba
Pradhumn <pradhumnagrawal14@gmail.com>
2022-12-07 13:43:31 +0530

    lec35 commit

--- src/Lec36/Person.java
+++ src/Lec36/Person.java
@@ -2,4 +2,14 @@ package Lec36;
 
 public class Person {
 
+    String name;
+    int age;
+
+    public void fun() {
+
+        int Aadhar_No = 901928;
+        System.out.println(this.name + " " + this.age + " " + Aadhar_No);
+
+    }
+
 }
---
+++ src/Lec36/Person_Client.java
@@ -0,0 +1,8 @@
+package Lec36;
+
+public class Person_Client {
+    public static void main(String[] args) {
+
+    }
+
+}

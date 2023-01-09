df17b235d4488c31a20c02e1b88b9fa02e383162
Pradhumn <pradhumnagrawal14@gmail.com>
2023-01-10 02:08:06 +0530

    commit

---
+++ src/Lec31/LinekdList_Client.java
@@ -0,0 +1,21 @@
+//package Lec31;
+//
+//public class LinekdList_Client {
+//    public static void main(String[] args) {
+//        LinkedList ll = new LinkedList();
+//        ll.addFirstPlace(1);
+//        ll.addFirstPlace(2);
+//        ll.addFirstPlace(3);
+//        ll.display();
+//        ll.addLast(-9);
+//        ll.AddAtIndex(80, 3);
+//        ll.display();
+////        System.out.println(ll.getFirst());
+////        System.out.println(ll.getLast());
+////        System.out.println(ll.getAtIndex(3));
+////        System.out.println(ll.removeFirst());
+////        System.out.println(ll.removeAtLast());
+//        System.out.println(ll.removeAtIndex(2));
+//        ll.display();
+//    }
+//}

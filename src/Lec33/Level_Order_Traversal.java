25f0bd1385d08b4a79d56cf68ad93798e4705b97
Pradhumn <pradhumnagrawal14@gmail.com>
2023-01-18 10:18:20 +0530

    lecture-33

--- src/Lec33/Level_Order_Traversal.java
+++ src/Lec33/Level_Order_Traversal.java
@@ -1,6 +1,9 @@
 package Lec33;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class Level_Order_Traversal {
     public class TreeNode {
@@ -24,7 +27,34 @@ public class Level_Order_Traversal {
 
     class Solution {
         public List<List<Integer>> levelOrder(TreeNode root) {
-            
+            Queue<TreeNode> q = new LinkedList<>();
+            Queue<TreeNode> q1 = new LinkedList<>();
+            List<List<Integer>> ans = new ArrayList<>();
+            List<Integer> ll = new ArrayList<>();
+
+            if (root == null) {
+                return ans;
+            }
+
+            q.add(root);
+
+            while (!q.isEmpty()) {
+                TreeNode rv = q.remove();
+                ll.add(rv.val);
+                if (rv.left != null) {
+                    q1.add(rv.left);
+                }
+                if (rv.right != null) {
+                    q1.add(rv.right);
+                }
+                if (q.isEmpty()) {
+                    ans.add(ll);
+                    ll = new ArrayList<Integer>();
+                    q = q1;
+                    q1 = new LinkedList<>();
+                }
+            }
+            return ans;
         }
     }
 

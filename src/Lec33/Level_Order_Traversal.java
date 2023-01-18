ee9eba60ef1b4a9b098e7755d8d597ee50fa723b
Pradhumn <pradhumnagrawal14@gmail.com>
2023-01-18 10:11:07 +0530

    lecture-33

---
+++ src/Lec33/Level_Order_Traversal.java
@@ -0,0 +1,31 @@
+package Lec33;
+
+import java.util.List;
+
+public class Level_Order_Traversal {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    class Solution {
+        public List<List<Integer>> levelOrder(TreeNode root) {
+            
+        }
+    }
+
+}
---
+++ src/Lec33/Main.java
@@ -0,0 +1,5 @@
+package Lec33;
+
+public class Main {
+
+}

9b3adc030f38cc8e81f8fa709750dd3a0352c511
Pradhumn <pradhumnagrawal14@gmail.com>
2023-01-19 09:21:59 +0530

    lecture-34

---
+++ src/Lec34/LCA.java
@@ -0,0 +1,27 @@
+package Lec34;
+
+public class LCA {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    class Solution {
+        public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+            if (root == q || root == q) {
+                
+            }
+
+            TreeNode left = lowestCommonAncestor(root.left, p, q);
+            TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        }
+    }
+
+}
